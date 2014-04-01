5bb2b6ef982b9ec108ac28da17a063f618106057
Eric Himmelreich <eric@rawsyntax.com>
2014-04-01 15:23:33 -0400

    Fix monitor to catch failures

--- jobs/ping_times.rb
+++ jobs/ping_times.rb
@@ -7,17 +7,19 @@ SCHEDULER.every '20s' do
 
   ['google.com', 'netflix.com', 'speedtest.net', 'comcast.net'].each do |site|
     val  = `ping -c 1 #{site}`
-    if val.present?
-      time = val.split('\n').last.split('=').last.split('/')[1].to_i.to_s + 'ms'
+    time = val.split('\n').last.split('=').last.split('/')[1].to_i.to_s + 'ms'
 
-      pings[site] = {label: site, value: time}
+    if time == "0ms"
+      ping_failures[Time.now.to_i] =
+        {label: site, value: Time.now.strftime('%I:%M%P')}
+
+      ping_failures.keys.select{ |k| k < 1.day.ago.to_i }
+        .map {|k| ping_failures.delete(k) }
     else
-      ping_failures[Time.now.to_i] = {label: site, value: Time.now.strftime('%I:%M%P')}
-      ping_failures.keys.select{ |k| k < 1.day.ago }.map {|k| ping_failures.delete(k) }
-
-      send_event('ping_failures', { items: ping_failures.values })
+      pings[site] = {label: site, value: time}
     end
   end
 
+  send_event('ping_failures', { items: ping_failures.values })
   send_event('ping_times', { items: pings.values })
 end
